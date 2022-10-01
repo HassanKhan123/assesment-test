ecb09a539253bd32411c0031e5303df89a9eacc2
HassanKhan123 <hk2602809@gmail.com>
2022-10-01 14:54:31 +0500

    Solution

--- src/rest-api.js
+++ src/rest-api.js
@@ -5,7 +5,6 @@ const express = require("express");
 // const restify = require('restify');
 
 module.exports = (stepService) => {
-  console.log("stepService", stepService.get());
   const REST_PORT = 8080;
 
   const app = express();
@@ -13,14 +12,18 @@ module.exports = (stepService) => {
   app.get("/users/:username/steps", (req, res) => {
     const params = req.params.username;
     const steps = stepService.get(params);
-    if (!steps) {
-      res.statusCode(404).json("User doesn not exist");
+
+    console.log("steps", steps);
+    if (!steps || !steps?.cumulativeSteps) {
+      return res.status(404).json({ error: "User doesn't exist" });
     }
     console.log("STEpsspspsp", params, steps);
-    res.send(steps);
+    return res.send(steps);
   });
 
-  app.listen(REST_PORT);
+  const server = app.listen(REST_PORT);
+
+  return server;
 
   // * TODO: Write the GET endpoint, using `stepService` for data access
   // * TODO: Return object containing `close()` method for shutting down the server
--- src/step.service.js
+++ src/step.service.js
@@ -3,9 +3,27 @@
 module.exports = function stepService(store) {
   const service = {};
 
-  service.get = (username) => store[username];
+  service.get = (username) => {
+    const user = store[username];
 
-  service.add = (username, ts, newSteps) => {
+    console.log("user========", typeof user);
+    if (typeof user === "function") return undefined;
+
+    return user ? user : undefined;
+  };
+
+  service.add = (username, ts, cumulativeSteps) => {
+    const user = store[username];
+
+    console.log(user, "==========");
+
+    if (!user) {
+      store[username] = { ts, cumulativeSteps };
+    } else {
+      user.cumulativeSteps = user.cumulativeSteps + cumulativeSteps;
+    }
+
+    return store;
     // Assume that `store` is initially an empty object {}. An example `store` is:
     // {
     //   jenna: {
